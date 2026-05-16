4af83d690cb14103cd0ecde5710c53dd33d26588
hyeonjiroh <sonamu6509@naver.com>
2026-05-17 03:56:51 +0900

    fix: home 폴더 import 경로 수정 및 CSS merge conflict 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/home";
-import PhotoGeneratePage from "./pages/photo-generate";
-
-function App() {
-=======
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home';
 import NotFoundPage from './pages/NotFound';
 import SpotPage from './pages/spot-detail';
+import PhotoGeneratePage from './pages/photo-generate';
 
 export default function App() {
->>>>>>> e8e7f9968bd48d939a8453ccae70db9307a7d263
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
 
-        <Route
-          path="/photo-generate"
-          element={<PhotoGeneratePage />}
-        />
-=======
+        <Route path="/photo-generate" element={<PhotoGeneratePage />} />
         <Route path="/spots/:spotId" element={<SpotPage />} />
         <Route path="*" element={<NotFoundPage />} />
->>>>>>> e8e7f9968bd48d939a8453ccae70db9307a7d263
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
